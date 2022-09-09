55a13f1e5c91abad38263188f7bb0e36d81ee391
Ivan <gubarenkovanek@gmail.com>
2022-09-09 22:12:54 +0300

    edits

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const gallery = document.querySelector('.gallery');
+
+
+const markup = galleryItems.map(({original,preview,description}) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href = ${original}>
+    <img 
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt= ${description}
+        />
+        </a>
+        </div>`).join("");
+        gallery.innerHTML = markup;
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Player ('iframe');
 
+player.on('timeupdate', throttle(e => {
+    localStorage.setItem('videoplayer-current-time',)
+}, 1000)
+);
+player
+    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    .catch(error => {
+        console.log(error);
+    })
