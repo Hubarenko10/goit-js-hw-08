09010175f33341e0702a5714551aebd770c6aba4
Ivan <gubarenkovanek@gmail.com>
2022-09-10 22:28:06 +0300

    task 2 && task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,13 @@
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Player ('iframe');
+const player = new Player (iframe);
 
 player.on('timeupdate', throttle(e => {
-    localStorage.setItem('videoplayer-current-time',)
-}, 1000)
-);
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+}, 1000));
 player
-    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-    .catch(error => {
-        console.log(error);
-    })
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))   
+.catch(error => {
+    console.log(error);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle'; 
+var throttle = require('lodash.throttle');
+const email = document.querySelector('input[name = "email"]');
+console.log(email);
+const message = document.querySelector('textarea[name = "message"]');
+console.log(message);
+const form = document.querySelector('.feedback-form');
+console.log(form);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(e => {
+    const objectToSave = { email: email.value, message: message.value };
+     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave))
+ },500));
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.reset();
+    console.log({ email: email.value, message: message.value });
+})
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+const storageData = load(LOCALSTORAGE_KEY);
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
 
