e28ca64b163a5a95a034bf5744e64349343e3abf
Ivan <gubarenkovanek@gmail.com>
2022-10-23 14:48:49 +0300

    edited

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player (iframe);
 
 player.on('timeupdate', throttle(e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
+    localStorage.setItem('videoplayer-current-time', e.seconds || 0);
 }, 1000));
 player
 .setCurrentTime(localStorage.getItem('videoplayer-current-time'))   
